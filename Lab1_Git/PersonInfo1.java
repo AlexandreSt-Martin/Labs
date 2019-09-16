d1763e9395a3c5bddde39e4149679ad4e997b4f0
PhilipeVR <prosi069@uottawa.ca>
2019-09-16 19:34:01 -0400

    PersonInfo1 Commit

--- Lab1_Git/PersonInfo1.java
+++ Lab1_Git/PersonInfo1.java
@@ -1,9 +1,9 @@
 public class PersonInfo1 {
 
-    public static String firstName = "Leisha";
-    public static String lastName = "Baker";
-    public static int studentNo = 338299;
-    public static String description = "Likes to take pictures";
+    public static String firstName = "Philipe";
+    public static String lastName = "Rosier";
+    public static int studentNo = 300005564;
+    public static String description = "Likes Soccer";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
